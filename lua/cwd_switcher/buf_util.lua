c9eb63cb84d7cb949861ce55f80d967001fdbef1
Prashantha SP <prashanth.sp98@gmail.com>
2023-08-26 20:58:56 +0530

    Buf utils script to handle open/create/close buffers

---
+++ lua/cwd_switcher/buf_util.lua
@@ -0,0 +1,30 @@
+local M = {}
+
+--- @return boolean
+M.close_open_buffers = function()
+    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+        print(vim.inspect(vim.api.nvim_buf_get_name(bufnr)))
+
+        if vim.api.nvim_buf_get_option(bufnr, "modified") then
+            local choice = vim.fn.confirm("This buffer has unsaved changes. close anyway?", "&Yes\n&No", 2)
+            if choice == 2 then
+                return false
+            else
+                local buf_name = vim.api.nvim_buf_get_name(bufnr)
+                print("Closing " .. buf_name)
+                vim.api.nvim_buf_delete(bufnr, { force = true })
+            end
+        else
+            vim.api.nvim_buf_delete(bufnr, { force = true })
+        end
+    end
+
+    return true
+end
+
+M.create_empty_buf = function()
+    local empty_buf = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_set_current_buf(empty_buf)
+end
+
+return M
